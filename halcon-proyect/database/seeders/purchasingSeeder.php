59bb88e9fd44117182e3587a059c95278d470d61
AFAP <81704152+afat3646@users.noreply.github.com>
2024-10-06 19:24:26 -0600

    Migrations, seeders & models created
    In the next commit i will send with the factories and seeders methods

---
+++ halcon-proyect/database/seeders/purchasingSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class purchasingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
